179a9420b47b7bf516fc2189f3a4ef50b5c2b178
Ranim <ranim.mouhamad@hotmail.com>
2023-10-01 15:54:17 +0200

    the Entities and FavouriteService classes have been documented

--- FavouriteManager/Persistence/Entity/Category.cs
+++ FavouriteManager/Persistence/Entity/Category.cs
@@ -3,12 +3,22 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FavouriteManager.Persistence.entity
 {
+    /// <summary>
+    /// This class represents a Category entity
+    /// </summary>
     [Table("Category")]
     public class Category
     {
+        /// <summary>
+        /// Gets or sets the unique id
+        /// </summary>
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the required label
+        /// </summary>
         [Required]
         [Column]
         public String Label { get; set; }
